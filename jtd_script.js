730ac17f2a7785f02c4ae5d544396f0e97dfb16e
nwaly <146173215+nwaly@users.noreply.github.com>
2026-03-25 17:09:49 +0100

    ajouter zones de texte et bouton

--- jtd_script.js
+++ jtd_script.js
@@ -1,4 +1,12 @@
+// pour afficher la date actuel en haut à gauche
 const d = new Date();
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 let name = month[d.getMonth()];
 document.getElementById("demo").innerHTML = d.getDate().toString()+" "+name+" "+d.getFullYear().toString();
+
+// Journal
+// Bouton
+function newEntry() {
+    const para = document.createElement("textarea");
+    document.body.getElementById("MainPage").appendChild(para);
+}
