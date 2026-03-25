08e4968eb4073104a4b811030355178d18c3f582
nwaly <146173215+nwaly@users.noreply.github.com>
2026-03-25 17:30:58 +0100

    update pages html

--- jtd_script.js
+++ jtd_script.js
@@ -2,11 +2,11 @@
 const d = new Date();
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 let name = month[d.getMonth()];
-document.getElementById("demo").innerHTML = d.getDate().toString()+" "+name+" "+d.getFullYear().toString();
+document.getElementById("today").innerHTML = d.getDate().toString()+" "+name+" "+d.getFullYear().toString();
 
 // Journal
 // Bouton
 function newEntry() {
     const para = document.createElement("textarea");
-    document.body.getElementById("MainPage").appendChild(para);
+    document.body.appendChild(para);
 }
