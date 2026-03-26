cb98189c9b6c53af812b139e4ad6ffc89cde14fc
nwaly <146173215+nwaly@users.noreply.github.com>
2026-03-26 11:29:42 +0100

    bottom bar position fixed !

--- jtd_script.js
+++ jtd_script.js
@@ -12,10 +12,20 @@ function newEntry() {
 }
 
 function valEntry() {
+    //crée un élément li sous la const 'newNode'
+    // = crée la balise dans laquelle la nouvelle entrée va être stockée 
     const newNode = document.createElement("li");
+    //associe l'input actuel à la const 'textNode'
     const textNode = document.getElementById("EntryInput");
+    //écrit le contenu du textNode (=de l'input actuel) dans la console? je crois?
     console.log(textNode);
+    //associe le contenu texte (value) du textNode (=de l'input actuel) à l'innerText du newNode (=de la balise de la nouvelle entrée)
     newNode.innerText = textNode.value;
+    //associe la balise de la liste d'entrée (ul) à la const 'list'
     const list = document.getElementById("EntryList");
+    //insère/ajoute newNode (=la nouvelle entrée) à la popsition [0] (=tout en haut) de la liste d'entrées 'list'
     list.insertBefore(newNode, list.children[0]);
+
+    // à faire mnt : 
+    // transformer le li en div/container joli avec le texte dedans
 }
