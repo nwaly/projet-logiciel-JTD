1f1ed4c4e7c6779281c706fa634202eea5ba175b
nwaly <146173215+nwaly@users.noreply.github.com>
2026-05-03 17:30:49 +0200

    adaptation avec flask

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/settings")
+def render_settings():
+    return render_template("settings.html")
+
+def main():
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
